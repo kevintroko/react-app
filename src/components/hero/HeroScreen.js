621af655335382ba8944ebd30f62c520a9f54479
Kevin Cabrera <kevintroko@gmail.com>
2022-03-02 12:14:00 -0600

    Hero screen done

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,19 +1,57 @@
 import React from 'react'
-import { Navigate, useParams } from 'react-router-dom'
+import { Navigate, useNavigate, useParams } from 'react-router-dom'
 import { getHeroById } from '../../selectors/getHeroById';
-import { HeroCard } from './HeroCard';
 
 export const HeroScreen = () => {
   const { heroId } = useParams();
   const hero = getHeroById(heroId);
 
+  const navigate = useNavigate();
+
   if (!hero) {
     return <Navigate to='/'/>
   }
 
+  const { id, superhero, publisher, alter_ego, first_appearance, characters } = hero;
+  
+  const handleReturn = () => {
+      navigate(-1);
+  };
+
+
+  const imagePath = `/assets/${id}.jpg`;
+
   return (
-    <div>
-      <HeroCard key={hero.id} {...hero}/>
+    <div className='row mt-5'>
+      <div className='col-4'>
+        <img
+          src={imagePath}
+          alt={superhero}
+          className="img-thumbnail"/>
+      </div>
+
+      <div className='col-8'>
+        <h3>{superhero}</h3>
+        <ul className='list-group list-group'>
+          <li className='list-group-item'>
+            <b>Alter hero: </b> {alter_ego}
+          </li>
+          <li className='list-group-item'>
+            <b>Publisher: </b> {publisher}
+          </li>
+          <li className='list-group-item'>
+            <b>First Appearance: </b> {first_appearance}
+          </li>
+          <li className='list-group-item'>
+            <b>Characters: </b> {characters}
+          </li>
+          <button
+            className='btn btn-outline-info mt-2'
+            onClick={handleReturn}>
+            Return
+          </button>
+        </ul>
+      </div>
     </div>
   )
 }
