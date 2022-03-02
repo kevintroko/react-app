972a949cf0a2d2fad90f5d165c29ada47ef825d4
Kevin Cabrera <kevintroko@gmail.com>
2022-03-01 18:26:35 -0600

    Fix errors from router v5

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
+    const handleLogout = () => {
+        console.log('Logout');
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             
@@ -15,32 +19,28 @@ export const Navbar = () => {
                 <div className="navbar-nav">
 
                     <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
+                        className={({ isActive }) => "nav-item nav-link btn " + (isActive && 'active')}
                         to="/marvel">
                         Marvel
                     </NavLink>
 
                     <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
+                        className={({ isActive }) => "nav-item nav-link btn " + (isActive && 'active')}
                         to="/dc">
                         DC
                     </NavLink>
                 </div>
             </div>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/login">
+                    <span className='nav-item nav-link text-info'>Kevin</span>
+
+                    <button 
+                        onClick={handleLogout}
+                        className="nav-item nav-link btn">
                         Logout
-                    </NavLink>
+                    </button>
                 </ul>
             </div>
         </nav>
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { DCScreen } from '../components/dc/DCScreen'
+import { LoginScreen } from '../components/login/LoginScreen'
+import { MarvelScreen } from '../components/marvel/MarvelScreen'
+import { SearchScreen } from '../components/search/SearchScreen'
+import { Navbar } from '../components/ui/Navbar'
+
+export const AppRouter = () => {
+  return (
+    <BrowserRouter>
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<MarvelScreen />} />
+            <Route path="/marvel" element={<MarvelScreen />} />
+            <Route path="/dc" element={<DCScreen />} />
+            <Route path="/search" element={<SearchScreen />} />
+            <Route path="/login" element={<LoginScreen />} />
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+/*
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+*/
