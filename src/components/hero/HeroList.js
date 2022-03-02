c9d5db521d2ad375544ecc4b30d8a10c410151b0
Kevin Cabrera <kevintroko@gmail.com>
2022-03-02 11:37:47 -0600

    Navigate if id is invalid

--- src/components/hero/HeroList.js
+++ src/components/hero/HeroList.js
@@ -3,15 +3,13 @@ import { getHeroesByPublisher } from '../../selectors/getHeroByPublisher';
 import { HeroCard } from './HeroCard';
 
 export const HeroList = ({publisher = 'Marvel Comics'}) => {
-    console.log(publisher);
-
-    const heores = getHeroesByPublisher(publisher);
+    const heros = getHeroesByPublisher(publisher);
 
     return (  
         <div className='row row-cols-1 row-cols-md-3'>
             <ul className='mt-4'>
                 {
-                    heores.map(hero => (
+                    heros.map(hero => (
                        <HeroCard key={hero.id} {...hero}/>
                     ))
                 }
--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Navigate, useParams } from 'react-router-dom'
+import { getHeroById } from '../../selectors/getHeroById';
+import { HeroCard } from './HeroCard';
 
 export const HeroScreen = () => {
+  const { heroId } = useParams();
+  const hero = getHeroById(heroId);
+
+  if (!hero) {
+    return <Navigate to='/'/>
+  }
+
   return (
-    <div>Hero</div>
+    <div>
+      <HeroCard key={hero.id} {...hero}/>
+    </div>
   )
 }
